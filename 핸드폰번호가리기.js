602f718f568df7c0331c04741d38692af084b623
sdc02408 <sdc02408@naver.com>
2022-01-17 23:57:37 +0900

    핸드폰번호가리기

---
+++ 핸드폰번호가리기.js
@@ -0,0 +1,18 @@
+// 문제 설명
+// 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+// 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+// 제한 조건
+// s는 길이 4 이상, 20이하인 문자열입니다.
+
+function solution(phone_number) {
+    var answer = '';
+    console.log(phone_number.slice(-4))
+
+    //4444로 변경 그앞에 값들은 어떻게 할지?
+    let changes = phone_number.slice(0,-4) 
+    console.log(changes.replace)
+    // console.log(changes.replaceAll('*'))
+
+    return answer;
+}
